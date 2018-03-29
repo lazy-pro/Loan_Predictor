a3c947dc9b4ba77cb711405dd48f6276deeed257
harsh <harshnayak1997@gmail.com>
2018-03-30 01:02:01 +0530

    first graph added

--- loan_predictor1.py
+++ loan_predictor1.py
@@ -9,6 +9,19 @@ df1 = pd.read_csv("F:/projects/Loan_Predictor/train_u6lujuX_CVtuZ9i.csv")
 y=df.Loan_Status
 print df1
 print df.describe()
+fig = plt.figure(figsize=(8,4))
+
+######################(graph--Probability of getting loan by credit history)
+temp1 = df1.pivot_table(values='Loan_Status',index=['Credit_History'],aggfunc=lambda x: x.map({'Y':1,'N':0}).mean())
+print '\nProbility of getting loan for each Credit History class:' 
+print temp1
+ax1 = temp1.plot(kind= 'bar')
+ax1.set_xlabel('Credit_History')
+ax1.set_ylabel('Probability of getting loan')
+ax1.set_title("Probability of getting loan by credit history")
+
+###############################################
+
 df = df[['Gender','Married','Dependents','Education','Self_Employed','ApplicantIncome','CoapplicantIncome','LoanAmount','Loan_Amount_Term','Credit_History','Property_Area']]
 
 train_features,test_features,train_labels,test_labels = train_test_split(df, y, test_size=0.2, shuffle=False)
